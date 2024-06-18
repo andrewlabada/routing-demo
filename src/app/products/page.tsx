972ad232535e34aef6b308e144a85cb591c8f8da
Drew <labadaandrew@gmail.com>
2024-06-19 02:14:39 +0800

    Dynamic Routes
    Work Content:
    - Create Dynamic routes with Product example
    - Define Params parameter with ProductId of String
    - Create folder with square bracket to determine id/params.
    
    PIC:
    Drew

---
+++ src/app/products/page.tsx
@@ -0,0 +1,10 @@
+export default function ProductList(){
+    return(
+    <>
+        <h1>Product List</h1>
+        <h2 className="text-3xl">Product 1</h2>
+        <h2 className="text-3xl">Product 2</h2>
+        <h2 className="text-3xl">Product 3</h2>
+    </>
+    );
+}
